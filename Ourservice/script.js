2a7efb415a4b829ca04fab34a51ab0540e89d415
Abhijeet kushwaha <32203719+abhianuj@users.noreply.github.com>
2018-07-02 16:50:40 +0530

    Update script.js

--- Ourservice/script.js
+++ Ourservice/script.js
@@ -26,3 +26,19 @@ explore.addEventListener('click', function(){
     let len = banner.offsetHeight + about.offsetHeight;
     window.scrollTo(0, len);
 })
+
+const dynamo = document.querySelector('.js');
+const text = "Divine Blue certification is a leading kolkata-based consultants.";
+let i=0;
+function heading() {
+    if(i < text.length){
+        dynamo.textContent = dynamo.textContent +  text.charAt(i);
+        if(i == (text.length-1)){
+            explore.style.opacity = '1';
+            explore.style.transform = 'translateY(0%)';
+        }
+        i++;
+        setTimeout(heading, 100);
+    }
+}
+setTimeout(heading, 2800);
